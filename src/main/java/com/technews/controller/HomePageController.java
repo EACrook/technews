5eed182aaf6012809517dfb4600603acaa792678
Emily Crook <Emily.A.Crook96@gmail.com>
2021-09-19 21:22:34 -0500

    added logout route as well as the homepage endpoint

--- src/main/java/com/technews/controller/HomePageController.java
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -1,6 +1,10 @@
 package com.technews.controller;
 
+import com.technews.model.Post;
+import com.technews.model.User;
 import com.technews.repository.CommentRepository;
+import com.technews.repository.PostRepository;
+import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +14,7 @@ import org.thymeleaf.Thymeleaf;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.swing.*;
+import java.util.List;
 
 @Controller
 public class HomePageController {
@@ -26,6 +31,12 @@ public class HomePageController {
     @Autowired
     CommentRepository commentRepository;
 
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/login")
 //    Model model is inherited functionality from Spring
     public String login(Model model, HttpServletRequest request) {
@@ -37,5 +48,39 @@ public class HomePageController {
 //        add attribute method is built into the Model object and sends info to the Thymeleaf templates--in this case it is sent to the string user
         model.addAttribute("user", new User());
         return "login";
+    }
+
+    @GetMapping("/users/logout")
+    public String logout(HttpServletRequest request) {
+        if (request.getSession(false) != null) {
+            request.getSession().invalidate();
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/")
+    public String homepageSetup(Model model, HttpServletRequest request) {
+        User sessionUser = new User();
+
+        if (request.getSession(false) != null) {
+            sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+            model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+        } else {
+            model.addAttribute("loggedIn", false);
+        }
+
+        List<Post> postList = postRepository.findAll();
+        for (Post p : postList) {
+            p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            User user = userRepository.getOne(p.getUserId());
+            p.setUserName(user.getUsername());
+        }
+
+        model.addAttribute("postList", postList);
+        model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+        model.addAttribute("point", "point");
+        model.addAttribute("points", "points");
+
+        return "homepage";
     }
 }
